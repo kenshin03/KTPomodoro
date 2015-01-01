c5e49befc3236f74ad177bf38a0ec25f89f2dcda
Kenny Tang <ktang@yahoo-inc.com>
2014-12-31 17:52:03 -0800

    Initial Commit

---
+++ KTPomodoro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KTPomodoro
+//
+//  Created by Kenny Tang on 12/31/14.
+//  Copyright (c) 2014 Kenny Tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KTPomodoro/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  KTPomodoro
+//
+//  Created by Kenny Tang on 12/31/14.
+//  Copyright (c) 2014 Kenny Tang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
